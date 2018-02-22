37a88bbfd0be30fa7d8d181bcaf011089721fae1
Larry Lu <pudding850806@gmail.com>
2018-02-22 12:29:54 +0800

    Load image file

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
 
+import smallImg from './assets/small.png'
+import bigImg from './assets/big.png'
+
 import Page1 from './page1'
 import Page2 from './page2'
 
@@ -8,6 +11,8 @@ const App = () => (
   <Fragment>
     <Page1 />
     <Page2 />
+    <img src={smallImg} alt="small image" />
+    <img src={bigImg} alt="big image" />
   </Fragment>
 )
 
--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,17 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
     ],
   },
   devServer: {
